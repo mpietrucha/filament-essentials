98b377f3577683887a6526a0a3b11407e7d5def8
Michał Pietrucha <mpietrucha50@gmail.com>
2026-02-16 19:49:59 +0100

    wip

---
+++ src/Commands/GenerateColors.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Mpietrucha\Filament\Essentials\Commands;
+
+use Illuminate\Console\Command;
+use Mpietrucha\Utility\Collection;
+use Mpietrucha\Utility\Enum;
+use Mpietrucha\Utility\Enums\Contracts\InteractsWithEnumInterface;
+use Mpietrucha\Utility\Filesystem;
+use Mpietrucha\Utility\Str;
+
+class GenerateColors extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'essentials:generate-colors
+                            {--input=App\Enums\Color : The Color enum class to generate CSS variables from}
+                            {--output=css/colors.css : The resource file to write the generated CSS to}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate CSS color variables';
+
+    public function handle(): int
+    {
+        /** @var string */
+        $input = $this->option('input');
+
+        if (Enum::incompatible($input)) {
+            Str::sprintf('The [%s] enum does not exist or does not implement [%s].', $input, InteractsWithEnumInterface::class) |> $this->error(...);
+
+            return Command::FAILURE;
+        }
+
+        $colors = $input::collection()->pipeThrough([
+            fn (Collection $colors) => $this->build(...) |> $colors->map(...),
+            fn (Collection $colors) => $colors->prepend('@theme {'),
+            fn (Collection $colors) => $colors->push('}'),
+            fn (Collection $colors) => Str::eol() |> $colors->join(...),
+        ]);
+
+        $output = $this->option('output') |> resource_path(...);
+
+        $this->lint($output);
+
+        Filesystem::put($output, $colors);
+
+        Str::sprintf('Colors generated successfully in [%s]', $output) |> $this->info(...);
+
+        return Command::SUCCESS;
+    }
+
+    protected function build(InteractsWithEnumInterface $color): string
+    {
+        $name = $color->key() |> Str::lower(...);
+
+        $value = $color->value();
+
+        return Str::sprintf('--color-%s: %s', $name, $value);
+    }
+
+    protected function lint(string $output): void
+    {
+
+    }
+}
---
+++ src/Commands/GeneratePolicies.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Mpietrucha\Filament\Essentials\Commands;
+
+use Filament\Facades\Filament;
+use Illuminate\Console\Command;
+use Mpietrucha\Utility\Filesystem;
+use Mpietrucha\Utility\Finder;
+use Mpietrucha\Utility\Str;
+use Mpietrucha\Utility\Type;
+
+class GeneratePolicies extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'essentials:generate-policies
+                            {--model=App\Models\User : The User model class to use in generated policies}
+                            {--panel=default : The panel ID to generate policies for}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate and configure Shield policies';
+
+    public function handle(): int
+    {
+        /** @var string */
+        $id = $this->option('panel');
+
+        $panel = match (true) {
+            $id === 'default' => Filament::getCurrentOrDefaultPanel(),
+            default => Filament::getPanel($id)
+        };
+
+        if (Type::null($panel)) {
+            Str::sprintf('Panel [%s] does not exist.', $id) |> $this->error(...);
+
+            return Command::FAILURE;
+        }
+
+        $panel->getId() |> $this->generate(...);
+
+        $this->rewrite();
+
+        $this->info('Policies generated successfully.');
+
+        return Command::SUCCESS;
+    }
+
+    protected function generate(string $id): void
+    {
+        $arguments = [
+            '--all' => true,
+            '--panel' => $id,
+            '--ignore-existing-policies' => true,
+            '--option' => 'policies_and_permissions',
+        ];
+
+        $this->callSilently('shield:generate', $arguments);
+    }
+
+    protected function rewrite(): void
+    {
+        $directory = app_path('Policies');
+
+        $model = $this->option('model');
+
+        Finder::uncached()
+            ->in($directory)
+            ->get()
+            ->keys()
+            ->each(fn (string $policy) => Filesystem::replaceInFile(
+                [
+                    'use Illuminate\Foundation\Auth\User as AuthUser;',
+                    'AuthUser',
+                    '$authUser',
+                ],
+                [
+                    Str::sprintf('use %s;', $model),
+                    'User',
+                    '$user',
+                ],
+                $policy
+            ));
+    }
+}
