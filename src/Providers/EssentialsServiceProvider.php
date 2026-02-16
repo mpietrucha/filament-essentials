9c23e69d9c2448963bf3c9391e0c759e4dc4e875
Michał Pietrucha <mpietrucha50@gmail.com>
2026-02-16 19:55:04 +0100

    wip

---
+++ src/Providers/EssentialsServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Mpietrucha\Filament\Essentials\Providers;
+
+use Mpietrucha\Filament\Essentials\Commands\GenerateColors;
+use Mpietrucha\Filament\Essentials\Commands\GeneratePolicies;
+use Mpietrucha\Laravel\Essentials\Package\Builder;
+use Mpietrucha\Laravel\Essentials\Package\ServiceProvider;
+
+class EssentialsServiceProvider extends ServiceProvider
+{
+    public function configure(Builder $package): void
+    {
+        $package->name('filament-essentials');
+
+        $package->hasConsoleCommands([
+            GenerateColors::class,
+            GeneratePolicies::class,
+        ]);
+    }
+}
