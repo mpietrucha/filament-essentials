f3df0d941e3c8a830cb1568967826840bb7f21ac
Michał Pietrucha <mpietrucha50@gmail.com>
2026-02-18 16:36:43 +0100

    wip

--- resources/views/components/select-option-with-avatar.blade.php
+++ resources/views/components/select-option-with-avatar.blade.php
@@ -1,6 +1,6 @@
 @props(['avatar'])
 
 <div class="flex items-center gap-x-2">
-    <img src="{{ $avatar }}" class="w-6 h-6 rounded-full object-cover" />
+    <img src="{{ $avatar }}" class="w-6 h-6 [.fi-badge-label-ctn_&]:w-4 [.fi-badge-label-ctn_&]:h-4 rounded-full object-cover" />
     <span>{{ $slot }}</span>
 </div>
