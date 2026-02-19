03329d67a1adabebaa237c542d3a96fb369f04c0
Michał Pietrucha <mpietrucha50@gmail.com>
2026-02-19 16:04:02 +0100

    wip

--- resources/views/components/select-option-with-avatar.blade.php
+++ resources/views/components/select-option-with-avatar.blade.php
@@ -2,5 +2,5 @@
 
 <div class="flex items-center gap-x-2">
     <img src="{{ $avatar }}" class="w-6 h-6 [.fi-badge-label-ctn_&]:w-4 [.fi-badge-label-ctn_&]:h-4 rounded-full object-cover" />
-    <span>{{ $slot }}</span>
+    <div class="min-w-0 truncate">{{ $slot }}</div>
 </div>
