956f52db6df281e47cd6ffa7212f15342af760b8
Michał Pietrucha <mpietrucha50@gmail.com>
2026-02-26 19:01:48 +0100

    wip

--- resources/lang/en/import.php
+++ resources/lang/en/import.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'completed' => '{0} Your :name import has completed and no rows imported.|{1} Your :name import has completed and :count row imported.|[2,*] Your :name import has completed and :count rows imported.',
+    'completed' => '{0} Your :name import has completed but no rows were imported.|{1} Your :name import has completed with :count row imported.|[2,*] Your :name import has completed with :count rows imported.',
     'failed' => '1 row failed to import.|:count rows failed to import.',
     'action' => [
         'label' => 'Import :Name',
