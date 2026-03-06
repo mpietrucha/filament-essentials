43645edc131c1e93af28ccc1671ccb8b2b170e3f
Michał Pietrucha <mpietrucha50@gmail.com>
2026-03-06 09:31:50 +0100

    wip

--- src/Mixins/ImageColumnMixin.php
+++ src/Mixins/ImageColumnMixin.php
@@ -2,15 +2,36 @@
 
 namespace Mpietrucha\Filament\Essentials\Mixins;
 
+use Mpietrucha\Utility\Backtrace;
 use Mpietrucha\Utility\Str;
+use Mpietrucha\Utility\Type;
 
 /**
  * @phpstan-require-extends \Filament\Tables\Columns\ImageColumn
  */
 trait ImageColumnMixin
 {
-    public function avatar(): static
+    public function avatar(?string $label = null): static
     {
-        return Str::none() |> $this->width('1%')->circular()->label(...);
+        $this->label(function () use ($label) {
+            if (Type::null($label)) {
+                return null;
+            }
+
+            $visible = Backtrace::get(DEBUG_BACKTRACE_IGNORE_ARGS, 10)
+                ->map
+                ->function()
+                ->contains('mapTableColumnToArray');
+
+            if ($visible) {
+                return $label;
+            }
+
+            return Str::none();
+        });
+
+        return $this->circular()->width('1%');
+
+        // return Str::none() |> $this->width('1%')->circular()->label(...);
     }
 }
