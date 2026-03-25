6aa1f8560ecc77c698c5845154f50c1aac01ce5d
Michał Pietrucha <mpietrucha50@gmail.com>
2026-03-25 16:34:59 +0100

    wip

--- src/Resources/Resource.php
+++ src/Resources/Resource.php
@@ -4,10 +4,8 @@ namespace Mpietrucha\Filament\Essentials\Resources;
 
 use Filament\Resources\Resource as FilamentResource;
 use Mpietrucha\Filament\Essentials\Resources\Concerns\InteractsWithActions;
-use Mpietrucha\Filament\Essentials\Resources\Concerns\TitlesRecordById;
 
 abstract class Resource extends FilamentResource
 {
     use InteractsWithActions;
-    use TitlesRecordById;
 }
