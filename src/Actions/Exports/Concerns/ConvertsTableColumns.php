65d3e2e406c234b71023b95044a59329970c9240
Michał Pietrucha <mpietrucha50@gmail.com>
2026-03-26 16:31:42 +0100

    wip

--- src/Actions/Exports/Concerns/ConvertsTableColumns.php
+++ src/Actions/Exports/Concerns/ConvertsTableColumns.php
@@ -3,9 +3,13 @@
 namespace Mpietrucha\Filament\Essentials\Actions\Exports\Concerns;
 
 use Filament\Actions\Exports\ExportColumn;
+use Filament\Actions\Exports\Exporter;
 use Filament\Tables\Columns\TextColumn;
 use Illuminate\Contracts\Support\Htmlable;
 
+/**
+ * @phpstan-require-extends Exporter
+ */
 trait ConvertsTableColumns
 {
     protected static function convertTextColumn(TextColumn $textColumn): ExportColumn
@@ -21,5 +25,16 @@ trait ConvertsTableColumns
 
             return $label->toHtml() |> strip_tags(...);
         });
+    }
+
+    /**
+     * @param  array<TextColumn>  $textColumns
+     * @return array<ExportColumn>
+     */
+    protected static function convertTextColumns(array $textColumns): array
+    {
+        $exportColumns = static::convertTextColumn(...) |> collect($textColumns)->map(...);
+
+        return $exportColumns->all();
     }
 }
