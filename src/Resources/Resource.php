17c8eb51e2e2f474ce1ae1ff9512e03c1ef5e987
Michał Pietrucha <mpietrucha50@gmail.com>
2026-03-27 20:06:02 +0100

    wip

--- src/Resources/Resource.php
+++ src/Resources/Resource.php
@@ -3,8 +3,16 @@
 namespace Mpietrucha\Filament\Essentials\Resources;
 
 use Filament\Resources\Resource as FilamentResource;
+use Filament\Resources\ResourceConfiguration;
+use Illuminate\Database\Eloquent\Model;
 use Mpietrucha\Filament\Essentials\Resources\Concerns\InteractsWithActions;
 
+/**
+ * @template TModel of Model = Model
+ * @template TConfiguration of ResourceConfiguration = ResourceConfiguration
+ *
+ * @extends FilamentResource<TModel, TConfiguration>
+ */
 abstract class Resource extends FilamentResource
 {
     use InteractsWithActions;
