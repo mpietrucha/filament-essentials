6166a165d507ac4703e75eae6ab1b1502cc75a26
Michał Pietrucha <mpietrucha50@gmail.com>
2026-08-19 17:46:10 +0200

    wip

--- src/Commands/GeneratePolicies.php
+++ src/Commands/GeneratePolicies.php
@@ -18,7 +18,8 @@ class GeneratePolicies extends Command
     #[\Override]
     protected $signature = 'essentials:generate-policies
                             {--model=App\Models\User : The User model class to use in generated policies}
-                            {--panel=default : The panel ID to generate policies for}';
+                            {--panel=default : The panel ID to generate policies for}
+                            {--policies= : The directory path to scan and rewrite generated policies (default: app/Policies)}';
 
     /**
      * @var string
@@ -65,7 +66,7 @@ class GeneratePolicies extends Command
 
     protected function rewrite(): void
     {
-        $directory = app_path('Policies');
+        $directory = $this->option('policies') ?? app_path('Policies');
 
         /** @var string */
         $model = $this->option('model');
