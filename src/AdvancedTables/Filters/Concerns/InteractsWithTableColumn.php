830c507a6791b9f0fc126ff58fa22d8ccb681c47
Michał Pietrucha <mpietrucha50@gmail.com>
2026-08-25 21:12:26 +0200

    wip

--- src/AdvancedTables/Filters/Concerns/InteractsWithTableColumn.php
+++ src/AdvancedTables/Filters/Concerns/InteractsWithTableColumn.php
@@ -4,11 +4,7 @@ namespace Mpietrucha\Filament\Essentials\AdvancedTables\Filters\Concerns;
 
 use Filament\Tables\Columns\Column;
 use Filament\Tables\Columns\TextColumn;
-use Mpietrucha\Filament\Essentials\AdvancedTables\Filters\Contracts\InteractsWithTableColumnInterface;
 
-/**
- * @phpstan-require-implements InteractsWithTableColumnInterface
- */
 trait InteractsWithTableColumn
 {
     public function getTableColumn(): Column
